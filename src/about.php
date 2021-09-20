a01a66bd90a6c77033099f74040548c4892f23d4
Nícolas Ribeiro Marques <56075706+NicolasRMarques@users.noreply.github.com>
2021-09-20 17:40:45 -0300

    Sobre podre

--- src/about.php
+++ src/about.php
@@ -12,3 +12,16 @@ $ALLNIME_Styler->setTemplate('about');
 $ALLNIME_Styler->output();
 
 ?>
+
+<html>
+  <head>
+    <h1>Bem-vindo!!!</h1>
+  </head>
+  <body>
+    Allnimes é uma comunidade criada por quatro estudantes de Ciência da Computação e um bicho preguiça, 
+    onde você pode encontrar informações dos seus desenhos favoritos (animes são desenhos, aceite) 
+    além de marcar o que você anda assistindo e já assistiu.<br>
+    Criado como o intuito de desenvolver um site para disciplina de <i>Desenvolvimento Web</i>, 
+    todo conteúdo é gratuito e acessível, onde a única limitação é o seu interesse.
+  </body>
+</html>
