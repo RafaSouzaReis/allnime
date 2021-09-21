097edf46ff8ccb0b690bf34eb06a98f2ff42f910
lniilisl <lniilisl@gmail.com>
2021-09-21 13:57:24 -0300

    Layout home

--- src/sys/config.php
+++ src/sys/config.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = '127.0.0.1';
-$db_username = 'root';
+$db_username = 'lixo';
 $db_password = 'root';
 $db_name = 'allnimes';
 $db_sqlcharset = 'utf-8';
--- src/sys/views/home.php
+++ src/sys/views/home.php
@@ -1,4 +1,53 @@
 <?php $this->includes('navbar'); ?>
-<div class="p-3"><?php $this->includes('trending'); ?></div>
-<div class="p-3"><?php $this->includes('recent'); ?></div>
-<div class="p-3"><?php $this->includes('recommended'); ?></div>
+<?php $this->includes('navbar'); ?>
+<div class="banner">
+  <div class="banner-contents">
+    <h1 class="banner-title">Mac Donalds</h1>
+    <div class="banner-buttons">
+      <buttons class="banner-button">Info</buttons>
+      <buttons class="banner-button">My List</butons>
+    </div>
+    <div class="banner-description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, sapiente?
+      Culpa cumque aliquid suscipit libero! Maxime nesciunt animi ipsam,
+      cupiditate libero vitae blanditiis sit quos excepturi. Maxime beatae sunt rerum.
+    </div>
+  </div>
+  <div class="banner-fadebottom">
+  </div>
+</div>
+<div class="poster-row">
+  <h2>Netflix Original</h2>
+  <div class="row-posters">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+  </div>
+</div>
+<div class="poster-row">
+  <h2>NSFW</h2>
+  <div class="row-posters">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+    <img src="./assets/img/test_poster.png" alt="macDonald" class="row-poster">
+  </div>
+</div>
+</div>
