d2106798d62bf673d39a5cb4f4e38d6c912cc175
João Pedro Dante <joaopdante@tryhosting.com.br>
2021-11-03 09:31:36 -0300

    Add alt

--- src/sys/views/includes/recent.php
+++ src/sys/views/includes/recent.php
@@ -3,40 +3,40 @@
   <div class="row-posters" id="recent-list">
     <button class="row-btn" onClick="turnLeft('recent-list')"><span><i class="fas fa-chevron-left"></i></span></button>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/7099/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/7099/large.jpg" alt="Capa My Little Monster">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/manga/poster_images/41222/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/manga/poster_images/41222/large.jpg" alt="Capa Sign">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/41971/original.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/41971/original.jpg" alt="Capa Boku No Hero">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/13209/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/13209/large.jpg" alt="Capa Black Clover">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/244/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/244/large.jpg" alt="Capa Bleach">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/42080/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/42080/large.jpg" alt="Capa Dr. Stone">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/41024/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/41024/large.jpg" alt="Capa Tensei shitara Slime Datta Ken">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/695/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/695/large.jpg" alt="Capa Castle Vania">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/1517/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/1517/large.jpg" alt="Capa Your Name">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/11999/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/11999/large.jpg" alt="Capa Your Name">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/100/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/100/large.jpg" alt="Capa FullMetal Alchemist">
     </a>
     <a class="row-poster" href="anime">
-      <img src="https://media.kitsu.io/anime/poster_images/9135/large.jpg" alt="macDonald">
+      <img src="https://media.kitsu.io/anime/poster_images/9135/large.jpg" alt="Capa Tokyo Ghoul">
     </a>
     <button class="row-btn row-btn-right" onClick="turnRight('recent-list')"><span><i class="fas fa-chevron-right"></i></span></button>
   </div>
